860e7ea234bdc74c0157b86eda3ed87f54c941f3
Franklin Tello <26192407+franklinjtc@users.noreply.github.com>
2023-10-23 22:46:05 -0500

    segundo commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,40 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+/*======= MENU SHOW =======*/
+/* validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
+/*======= MENU HIDDEN =======*/
+/* validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
 
 
-/*=============== SWIPER FAVORITES ===============*/ 
+/*=============== SWIPER FAVORITES ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
